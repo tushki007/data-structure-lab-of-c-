d80a38e77b0cc9e03067f8bfcd2efde8d17f1e40
tushki007 <tusharsingh2322@gmail.com>
2025-02-27 12:48:59 +0530

    sorting.c

---
+++ sorting.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void sortArray(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void displayArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[100], size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    sortArray(arr, size);
+
+    printf("Sorted array: ");
+    displayArray(arr, size);
+
+    return 0;
+}
